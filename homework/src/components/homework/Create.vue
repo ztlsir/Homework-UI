28dea02b10b3f9453321531fe7b5846c4c5069c0
ztlsir <584821699@qq.com>
2020-04-24 19:21:23 +0800

    complete homework.Create

--- homework/src/components/homework/Create.vue
+++ homework/src/components/homework/Create.vue
@@ -3,6 +3,30 @@
     <h1>创建作业</h1>
     <h3>作文：</h3>
     <el-form :rules="rules" ref="form" :model="form" label-width="80px">
+      <el-form-item label="老师" prop="teacherId">
+        <el-select v-model="form.teacherId" placeholder="请选择老师">
+          <el-option label="李老师" value="000001"></el-option>
+          <el-option label="刘老师" value="000002"></el-option>
+          <el-option label="张老师" value="000003"></el-option>
+          <el-option label="王老师" value="000004"></el-option>
+          <el-option label="姚老师" value="000005"></el-option>
+          <el-option label="曹老师" value="000006"></el-option>
+        </el-select>
+      </el-form-item>
+      <el-form-item label="班级" required>
+        <el-col :span="10">
+          <el-form-item prop="classes">
+            <el-input type="text" v-model.number="form.classes" autocomplete="off"></el-input>
+          </el-form-item>
+        </el-col>
+        <el-col class="line" :span="2">年级</el-col>
+        <el-col :span="10">
+          <el-form-item prop="grade">
+            <el-input type="text" v-model.number="form.grade" autocomplete="off"></el-input>
+          </el-form-item>
+        </el-col>
+        <el-col class="line" :span="2">班</el-col>
+      </el-form-item>
       <el-form-item label="描述" prop="description">
         <el-input type="textarea" v-model="form.description"></el-input>
       </el-form-item>
@@ -15,15 +39,23 @@
           :auto-upload="true"
           :limit="1"
           :file-list="fileList"
+          :on-success="uploadOnSuccess"
         >
           <i slot="default" class="el-icon-plus"></i>
           <div slot="file" slot-scope="{file}">
             <img class="el-upload-list__item-thumbnail" :src="file.url" alt />
             <span class="el-upload-list__item-actions">
-              <span class="el-upload-list__item-preview" @click="handlePictureCardPreview(file)">
+              <span
+                class="el-upload-list__item-preview"
+                @click="uploadHandlePictureCardPreview(file)"
+              >
                 <i class="el-icon-zoom-in"></i>
               </span>
-              <span v-if="!disabled" class="el-upload-list__item-delete" @click="handleRemove()">
+              <span
+                v-if="!disabled"
+                class="el-upload-list__item-delete"
+                @click="uploadHandleRemove()"
+              >
                 <i class="el-icon-delete"></i>
               </span>
             </span>
@@ -51,39 +83,82 @@ export default {
       disabled: false,
       fileList: [],
       form: {
+        classes: 1,
+        grade: 1,
+        teacherId: "",
         description: "",
-        imagePath: ""
+        imageId: ""
       },
       rules: {
+        teacherId: [
+          { required: true, message: "请选择老师" , trigger: "blur"}
+        ],
         description: [
-          { required: true, message: "请输入描述信息", trigger: "blur" },
+          { required: true, message: "请输入描述信息"},
           {
             min: 10,
             max: 9999,
-            message: "长度在 10 到 9999 个字符",
-            trigger: "blur"
+            message: "长度在 10 到 9999 个字符"
           }
+        ],
+        classes: [
+          { required: true, message: "年级不能为空" },
+          { type: "number", message: "年级必须为数字值" }
+        ],
+        grade: [
+          { required: true, message: "班号不能为空" },
+          { type: "number", message: "班号必须为数字值" }
         ]
       }
     };
   },
   methods: {
-    handleRemove() {
+    uploadHandleRemove() {
       this.fileList.pop();
+      this.resetImageId();
     },
-    handlePictureCardPreview(file) {
+    uploadHandlePictureCardPreview(file) {
       this.dialogImageUrl = file.url;
       this.dialogVisible = true;
+    },
+    uploadOnSuccess(response) {
+      this.form.imageId = response.id;
     },
     onSubmit(formName) {
       this.$refs[formName].validate(valid => {
         if (valid) {
-          alert("submit!");
+          return this.$http
+            .post("/homework-api/homeworks",this.composeSubmitData())
+            .then(function(response) {
+              alert("success:"+JSON.stringify(response));
+            })
+            .catch(function(error) {
+              alert("fail:"+JSON.stringify(error));
+            });
         } else {
           console.log("error submit!!");
           return false;
         }
       });
+    },
+    composeSubmitData() {
+      var content = {
+        description: this.form.description,
+        imageId: this.form.imageId
+      };
+      var submitData = {
+        classAndGrade: {
+          classes: this.form.classes,
+          grade: this.form.grade
+        },
+        teacherId: this.form.teacherId,
+        content: JSON.stringify(content)
+      };
+
+      return submitData;
+    },
+    resetImageId() {
+      this.form.imageId = "";
     }
   }
 };
--- homework/src/main.js
+++ homework/src/main.js
@@ -1,8 +1,10 @@
 import Vue from 'vue'
 import Homework from './Homework.vue'
 import './plugins/element.js'
+import axios from 'axios'
 
 Vue.config.productionTip = false
+Vue.prototype.$http= axios
 
 new Vue({
   render: h => h(Homework),
